4c1a1d42f986a77af0c4e312168a63edd0d490e2
Brandon Vasquez <bvasquez@Brandons-MBP.cable.rcn.com>
2021-12-05 11:47:36 -0500

    Cleaned up day3p2 because there was a lot of redundant code

--- d03p2/src/main.rs
+++ d03p2/src/main.rs
@@ -1,36 +1,27 @@
 use std::fs;
 
+fn calculate(bitwidth : usize, mut data :Vec<Vec<u32>>, mode : bool) -> u32 {
+    let mut half_count;
+    for i in 0..bitwidth {
+        if data.len() == 1 { break;}
+        half_count = data.len() as f32 / 2.0;
+        let bit_sum = (data.iter().fold(0, |acc, x| acc + x[i]) as f32 / half_count) as u32;
+        data.retain(|x| x[i] == if mode {bit_sum} else {1-(bit_sum%2)});
+    }
+    data[0].iter().fold(0, |acc, x| acc << 1 | *x as u32)
+}
+
 fn main() {
-    let data = fs::read_to_string("input.txt").expect("File path missing");
+    let data = fs::read_to_string("example.txt").expect("File path missing");
     let vector_of_bits_ref: Vec<Vec<u32>> = data
         .lines()
         .map(|x| x.chars().map(|x| x.to_digit(10).unwrap()).collect())
         .collect();
     let bitwidth = vector_of_bits_ref[0].len();
-    let count = vector_of_bits_ref.len();
-
-    let mut half_count : f32;
 
     let mut bits_copy = vector_of_bits_ref.clone();
-    for i in 0..bitwidth {
-        if bits_copy.len() == 1 { break;}
-        half_count = bits_copy.len() as f32 / 2.0;
-        let bit_sum = (bits_copy.iter().fold(0, |acc, x| acc + x[i]) as f32 / half_count) as u32;
-        bits_copy.retain(|x| x[i] == bit_sum.try_into().unwrap());
-    }
-    let oxy_number = bits_copy[0].iter().fold(0, |acc, x| acc << 1 | *x as u32);
-
-    let mut bits_copy = vector_of_bits_ref.clone();
-    for i in 0..bitwidth {
-        if bits_copy.len() == 1 { break;}
-        half_count = bits_copy.len() as f32 / 2.0;
-        let mut bit_sum = (bits_copy.iter().fold(0, |acc, x| acc + x[i]) as f32 / half_count) as u32;
-        bit_sum = 1 - (bit_sum%2);
-        bits_copy.retain(|x| x[i] == bit_sum);
-    }
-    let co2_number = bits_copy[0].iter().fold(0, |acc, x| acc << 1 | *x as u32);
-
-    println!("oxygen {:?}", oxy_number);
-    println!("co2 {:?}", co2_number);
+    let oxy_number = calculate(bitwidth, bits_copy, true);
+    bits_copy = vector_of_bits_ref.clone();
+    let co2_number = calculate(bitwidth, bits_copy, true);
     println!("prod {:?}", oxy_number * co2_number);
 }
